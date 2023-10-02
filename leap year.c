63f73c87c87f1fc7040e48dc0f485608af8d3ccc
RAGULRAJ12345 <146200452+RAGULRAJ12345@users.noreply.github.com>
2023-10-01 21:27:08 -0700

    LEAP YEAR
    CREATED BY RAGUL RAJ

---
+++ leap year.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("=> HI WELCOME TO 'LEAP YEAR 'FINDING APP.\n\n=> THIS APP WAS INVENTED BY R.RAGULRAJ.\n\n=>THIS IS VERSION 1.1 \n\n");
+	
+	int year;
+	printf("=>NOW ENTER THE YEAR ______");
+	scanf("%d",&year);
+	if(year%4==0)
+	{
+		printf("=>YOUR YEAR IS A LEAP YEAR!!!!!\n\n=>THANKYOU FOR DOWLOADING THE APP\n\n=>THE APP FOR DEVELOPED BY 'DONAK <^>'");
+		
+		
+	}
+	else{
+		printf("=>YOUR YEAR IS NOT A LEAP YEAR \n\n=>THANKYOU FOR DOWLOADING THE APP\n\n=>THE APP FOR DEVELOPED BY 'DONAK <^>'");
+	}
+	
+	
+	
+}
+	
+	
+	return 0;
+}
